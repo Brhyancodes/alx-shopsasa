18d75392229a290b1d270c5f3a119a108630ac5c
Brian Wakhale <brianweke1998@gmail.com>
2025-03-09 18:36:37 +0300

    feat: add social media link to foote

--- alx-shop-sasa/layout/Footer.tsx
+++ alx-shop-sasa/layout/Footer.tsx
@@ -4,7 +4,7 @@ import FooterNav from "@/components/common/FooterNav";
 import CompanyInfo from "@/components/common/CompanyInfo";
 import { CompanyInfoProps } from "@/interfaces";
 import { IoHome, IoMail } from "react-icons/io5";
-import { FaPhoneAlt } from "react-icons/fa";
+import { FaPhoneAlt, FaTwitter, FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const info: CompanyInfoProps[] = [
   {
@@ -27,6 +27,7 @@ const info: CompanyInfoProps[] = [
 function Footer() {
   return (
     <footer className={`${nunito.className}`}>
+      {/* Top Section */}
       <div className="bg-[#008080] grid sm:grid-cols-3 gap-12 py-10 pl-6 lg:py-14 lg:px-12 text-[#FAFAFA]">
         <Subscription />
         <FooterNav />
@@ -37,8 +38,25 @@ function Footer() {
         </div>
       </div>
 
-      <div className="flex justify-center items-center bg-[#1E293B] text-[#FAFAFA] text-sm py-4">
-        <p>&copy; Copyright {new Date().getFullYear()} All rights reserved.</p>
+      {/* Bottom Section */}
+      <div className="bg-[#1E293B] text-[#FAFAFA] text-sm py-4 flex flex-col items-center">
+        {/* Social Media Links */}
+        <div className="flex space-x-4 mb-2">
+          <a href="https://twitter.com/briancodes_" target="_blank" rel="noopener noreferrer">
+            <FaTwitter className="text-xl hover:text-[#1DA1F2] transition duration-300" />
+          </a>
+          <a href="https://instagram.com/brhyancodes_" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="text-xl hover:text-[#E1306C] transition duration-300" />
+          </a>
+          <a href="https://github.com/Brhyancodes" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="text-xl hover:text-gray-400 transition duration-300" />
+          </a>
+          <a href="https://www.linkedin.com/in/brian-wakhale-896b6b2a3/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="text-xl hover:text-[#0077B5] transition duration-300" />
+          </a>
+        </div>
+
+        <p>&copy; {new Date().getFullYear()} All rights reserved.</p>
       </div>
     </footer>
   );
