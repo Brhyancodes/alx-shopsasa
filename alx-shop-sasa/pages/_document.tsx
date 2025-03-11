c6f9ffc0e7b7b08dff7418656e2929d60fbdcc60
Brian Wakhale <brianweke1998@gmail.com>
2025-03-11 06:54:11 +0300

    chore: general maintenance and PWA improvements

--- alx-shop-sasa/pages/_document.tsx
+++ alx-shop-sasa/pages/_document.tsx
@@ -8,10 +8,10 @@ export default function Document() {
         {/* Manifest for PWA */}
         <link rel="manifest" href="/manifest.json" />
         {/* PWA Icon */}
-        <link rel="icon" href="/icons/icon-192x192.png" sizes="192x192" />
-        <link rel="icon" href="/icons/icon-512x512.png" sizes="512x512" />
+        <link rel="icon" href="/public/images/android-chrome-192x192.png" sizes="192x192" />
+        <link rel="icon" href="/public/images/android-chrome-512x512.png" sizes="512x512" />
         {/* Apple Icons */}
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/public/images/apple-touch-icon.png" />
         {/* Theme Color */}
         <meta name="theme-color" content="#008080" />
       </Head>
